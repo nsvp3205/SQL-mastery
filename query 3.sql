2fd0559138e2041949ab572f1997b48176af3608
nagasaivaraprasad <nsvprasad3205@gmail.com>
2025-12-24 16:14:56 +0530

    dec 24: sql practice and notes completed

--- query 3.sql
+++ query 3.sql
@@ -1,7 +1,13 @@
+--Goal: Find the total number of Action movies released in 2010.
+
 SELECT
+  -- Count all rows that match the criteria and rename the column for clarity
   count(*) AS total_movies
 FROM
+  -- Specify the source table
   movie
 WHERE
+  -- Filter for the specific genre
   genre = "Action"
+  -- Extract the year from the release_date column and compare it to 2010
   AND strftime('%Y', release_date) = "2010";
